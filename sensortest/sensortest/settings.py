e29d0d1d9e1d6450fd50783fbc3098d735012d9b
Prudhvinik1 <prudhvikrishna.n16@iiits.in>
2017-08-31 08:35:41 +0530

    Add files via upload

--- sensortest/sensortest/settings.py
+++ sensortest/sensortest/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'sense.apps.senseConfig',
+    #'sense.apps.senseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
